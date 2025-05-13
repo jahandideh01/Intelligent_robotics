4133e434e16d92c301d7d6caa12a9c93f436d65b
Amin <jahandideh01@gmail.com>
2025-05-13 11:16:39 -0700

    Backup before modifying node_a

--- src/node_a.cpp
+++ src/node_a.cpp
@@ -1,33 +1,50 @@
 #include <ros/ros.h>
-#include <ir2425_group_16/ApriltagIDs.h>
-#include <ir2425_group_16/Coeffs.h>  // Include the service header
+#include <std_msgs/String.h>
+#include <ir2425_group_16/GetApriltagIDs.h>  // Replace with your actual service
 
-int main(int argc, char** argv) {
+ros::Publisher target_pub;
+
+void feedbackCallback(const std_msgs::String::ConstPtr& msg) {
+    ROS_INFO("[Node A] Feedback from Node B: %s", msg->data.c_str());
+}
+
+int main(int argc, char **argv) {
     ros::init(argc, argv, "node_a");
     ros::NodeHandle nh;
 
-    ros::Publisher id_pub = nh.advertise<ir2425_group_16::ApriltagIDs>("tag_ids", 10);
-    ros::ServiceClient client = nh.serviceClient<ir2425_group_16::Coeffs>("/apriltags_ids_srv");
+    // Setup publisher and subscriber
+    target_pub = nh.advertise<std_msgs::String>("/target_ids", 10);
+    ros::Subscriber feedback_sub = nh.subscribe("/node_b_feedback", 10, feedbackCallback);
 
-    ros::Rate loop_rate(1);
-    while (ros::ok()) {
-        ir2425_group_16::ApriltagIDs msg;
-        ir2425_group_16::Coeffs srv;
-        srv.request.ready = true;
+    // Wait for service to be available
+    ROS_INFO("[Node A] Waiting for /apriltag_ids_srv service...");
+    ros::service::waitForService("/apriltag_ids_srv");
+    ROS_INFO("[Node A] Service available, calling...");
 
-        if (client.call(srv)) {
-            for (float coeff : srv.response.coeffs) {
-                msg.ids.push_back(static_cast<int>(coeff));
-            }
-            id_pub.publish(msg);
-            ROS_INFO("Node A published IDs from service.");
-        } else {
-            ROS_WARN("Failed to call service /apriltags_ids_srv");
+    // Call the service
+    ros::ServiceClient client = nh.serviceClient<ir2425_group_16::GetApriltagIDs>("/apriltag_ids_srv");
+    ir2425_group_16::GetApriltagIDs srv;
+    
+    
+    srv.request.ready = true;
+    if (client.call(srv)) {
+        std_msgs::String msg;
+        std::stringstream ss;
+        for (size_t i = 0; i < srv.response.ids.size(); ++i) {
+            ss << srv.response.ids[i];
+            if (i != srv.response.ids.size() -1) ss << ",";
         }
-
-        ros::spinOnce();
-        loop_rate.sleep();
+        msg.data = ss.str();
+        ROS_INFO("[Node A] Received and publishing IDs: %s", msg.data.c_str());
+        target_pub.publish(msg);
+    } else {
+        ROS_ERROR("[Node A] Failed to call /apriltag_ids_srv");
     }
+
+    // Keep node alive to listen for feedback
+    ROS_INFO("[Node A] Waiting for feedback from Node B...");
+    ros::spin();
+
     return 0;
 }
 
--- src/node_b.cpp
+++ src/node_b.cpp
@@ -1,18 +1,91 @@
 #include <ros/ros.h>
-#include <ir2425_group_16/ApriltagIDs.h>
+#include <std_msgs/String.h>
+#include <sstream>
 
-void idsCallback(const ir2425_group_16::ApriltagIDs::ConstPtr& msg) {
-    ROS_INFO("Node B received IDs:");
-    for (auto id : msg->ids) {
-        ROS_INFO(" - ID: %d", id);
+#include <visualization_msgs/MarkerArray.h>
+#include <apriltag_ros/AprilTagDetectionArray.h>
+#include <apriltag_ros/AprilTagDetection.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf/transform_listener.h>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+
+std::string received_ids;
+
+std::vector<int> target_ids;  // Received from Node A
+ros::Publisher feedback_pub;
+tf::TransformListener* tf_listener;
+
+void targetIdsCallback(const std_msgs::String::ConstPtr& msg) {
+    target_ids.clear();
+    std::stringstream ss(msg->data);
+    std::string id_str;
+
+    while (std::getline(ss, id_str, ',')) {
+        try {
+            int id = std::stoi(id_str);
+            target_ids.push_back(id);
+        } catch (...) {
+            ROS_WARN("Invalid ID received: %s", id_str.c_str());
+        }
+    }
+
+    ROS_INFO("[Node B] Received target IDs:");
+    for (int id : target_ids) {
+        ROS_INFO("- %d", id);
     }
 }
 
-int main(int argc, char** argv) {
+
+void tagDetectionsCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg) {
+    for (const auto& detection : msg->detections) {
+        int id = detection.id[0];
+        
+	ROS_INFO("[Node B] tagDetectionsCallback triggered with %lu detections", msg->detections.size());
+
+        if (std::find(target_ids.begin(), target_ids.end(), id) != target_ids.end()) {
+            ROS_INFO("[Node B] Found target ID: %d", id);
+
+            geometry_msgs::PoseStamped camera_pose;
+            camera_pose.header = detection.pose.header;  // use the same timestamp/frame
+            camera_pose.pose = detection.pose.pose.pose; // get the actual pose (without covariance)
+
+            // Transform to map frame
+            geometry_msgs::PoseStamped map_pose;
+
+            try {
+                tf_listener->transformPose("map", camera_pose, map_pose);
+                ROS_INFO("[Node B] ID %d in map frame: [%.2f, %.2f, %.2f]",
+                         id,
+                         map_pose.pose.position.x,
+                         map_pose.pose.position.y,
+                         map_pose.pose.position.z);
+            } catch (tf::TransformException &ex) {
+                ROS_WARN("Transform error: %s", ex.what());
+            }
+        }
+    }
+
+    std_msgs::String feedback;
+    feedback.data = "Scanning tags...";
+    feedback_pub.publish(feedback);
+}
+
+
+int main(int argc, char **argv) {
     ros::init(argc, argv, "node_b");
     ros::NodeHandle nh;
-    ros::Subscriber id_sub = nh.subscribe("tag_ids", 10, idsCallback);
+
+    tf_listener = new tf::TransformListener;
+
+    ros::Subscriber ids_sub = nh.subscribe("/target_ids", 10, targetIdsCallback);
+    ros::Subscriber tag_sub = nh.subscribe("/tag_detections", 10, tagDetectionsCallback);
+    feedback_pub = nh.advertise<std_msgs::String>("/node_b_feedback", 10);
+
     ros::spin();
+    
     return 0;
 }
 
